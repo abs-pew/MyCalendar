8c5f5e6338f4ce60d40142542ee16e454307dacf
abdulsalam <abs.pew@gmail.com>
2025-06-26 17:16:20 +0200

    almost there but static - Main Class

--- src/Main.java
+++ src/Main.java
@@ -7,36 +7,51 @@ public class Main {
         int[] monthDaysCount = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         String[] dayNames = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 
+        int calendarYear = 2025;
+        //int yearTotalDaysCount = 1;
+        int newLineCounter = 3;
+
+// boolean isNextMonth = ;
         System.out.println();
         int monthDaysIndex = 0;
         for(String monthName : monthNames)
         {
 
             System.out.println(monthName);
-            int weekDaysIndex = 0;
+           // int weekDaysIndex = 0;
             for(String dayName : dayNames)
             {
 
                 System.out.print(dayName.substring(0, 2) + " ");
-                weekDaysIndex++;
+            //    weekDaysIndex++;
             }
-
             System.out.println();
+            setNewLinePointer(newLineCounter);
 
             int i = 1;
             while (i  <= monthDaysCount[monthDaysIndex])
             {
                 System.out.print(String.format("%1$2s", i) + " ");
-                if(i % 7 == 0) {
+                if(newLineCounter % 7 == 0) {
                     System.out.println();
+                   //newLineCounter = 0;  // it gets incremented to 1 for next iteration of While-Loop
                 }
                 // System.out.print(i + " ");
 
                 i++;
+                newLineCounter++;
             }
             System.out.println();
             System.out.println();
             monthDaysIndex++;
+
+        }
+    }
+    public static void setNewLinePointer(int newLineCounter)
+    {
+        for (int j = 1; j < (newLineCounter % 7); j++)
+        {
+            System.out.print(String.format("%1$2s", " ") + " ");
         }
     }
 }
